73f08eafeb01c533bf66744fc7231c69a1fc9266
Adam Beck <adamb3189@gmail.com>
2018-02-09 09:32:57 +1100

    test: intial tests

--- test/please_stop_test.exs
+++ test/please_stop_test.exs
@@ -1,4 +1,13 @@
 defmodule PleaseStopTest do
   use ExUnit.Case
-  doctest PleaseStop
+
+  describe "when a value is given to init" do
+    test "it returns the provided value" do
+      value = %{}
+
+      assert(PleaseStop.init(value)) === value
+    end
+  end
+
+
 end
